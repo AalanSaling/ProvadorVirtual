a0bf8dc93646316f23bfa8b1a29381d113b2cc5a
AalanSaling <saling.levelup@gmail.com>
2026-08-18 08:25:40 -0300

    chore: rotate secrets and update build artifacts
    Rotate encrypted secrets in the vault, clean up the Metro configuration, and update web build distribution files.

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.serializer = {
-  ...config.serializer,
-  getPolyfills: () => [],
-};
-
 module.exports = config;
+
+
